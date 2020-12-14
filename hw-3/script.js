3c82676f963e78c9465a26fe37968f10d686db08
Mihailo Jacimovic <mihajac98@gmail.com>
2020-12-14 17:11:16 +0100

    Stilizovao glavnu stranicu i dodao dugme za vracanje nazad u forma.html

--- hw-3/script.js
+++ hw-3/script.js
@@ -1,10 +1,15 @@
 function zapocniKviz() {
     location.replace("http://127.0.0.1:5500/stranica_sa_pitanjima.html")
 }
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
+function otvoriFormu() {
+  location.replace("http://127.0.0.1:5500/forma.html")
   }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
+function vratiNazad() {
+  let provera = document.getElementById("ime");
+  if(!provera.checkValidity()){
+    location.replace("http://127.0.0.1:5500/glavna_stranica.html"); 
   }
+  else {
+    location.replace("http://127.0.0.1:5500/glavna_stranica.html");
+  }
+}
