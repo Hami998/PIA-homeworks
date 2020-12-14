265f808e4767c8ee0e65dc1e33c4fdce489a10d7
Mihailo Jacimovic <mihajac98@gmail.com>
2020-12-14 13:01:53 +0100

    Napravio sam formu za registraciju

---
+++ hw-3/script.js
@@ -0,0 +1,10 @@
+function zapocniKviz() {
+    location.replace("http://127.0.0.1:5500/stranica_sa_pitanjima.html")
+}
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
