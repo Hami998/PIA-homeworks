620fc795a623e72b862deb370c8cc3412399c6e2
Mihailo Jacimovic <mihajac98@gmail.com>
2020-12-14 18:11:52 +0100

    Stilizovao dodatno stanicu pravila igre i povezao je sa glavnom stranicom

--- hw-3/script.js
+++ hw-3/script.js
@@ -1,9 +1,6 @@
 function zapocniKviz() {
-    location.replace("http://127.0.0.1:5500/stranica_sa_pitanjima.html")
+    location.replace("http://127.0.0.1:5500/stranica_sa_pitanjima.html");
 }
-function otvoriFormu() {
-  location.replace("http://127.0.0.1:5500/forma.html")
-  }
 function vratiNazad() {
   let provera = document.getElementById("ime");
   if(!provera.checkValidity()){
@@ -13,3 +10,15 @@ function vratiNazad() {
     location.replace("http://127.0.0.1:5500/glavna_stranica.html");
   }
 }
+function vratiForma() {
+  location.replace("http://127.0.0.1:5500/forma.html");
+}
+function vratiPocetna() {
+  location.replace("http://127.0.0.1:5500/glavna_stranica.html");
+}
+function otvoriPravilaIgre() {
+  location.replace("http://127.0.0.1:5500/pravila_igre.html");
+}
+function otvoriFormu() {
+  location.replace("http://127.0.0.1:5500/forma.html");
+  }
