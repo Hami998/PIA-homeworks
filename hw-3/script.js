aef364a7750e42799f6d55e0e1d886062d3d8b45
Mihailo Jacimovic <mihajac98@gmail.com>
2020-12-17 21:11:20 +0100

    Namestio da se posle prvog pitanja ucita drugo

--- hw-3/script.js
+++ hw-3/script.js
@@ -24,8 +24,7 @@ function otvoriFormu() {
   location.replace("http://127.0.0.1:5500/forma.html");
 }
 // funkcije za rad sa pitanjima
-function ucitajPitanje1() {
-  //$(".tekst").remove();
+function ucitajPrvoPitanje(){
   $.getJSON('pitanja.json', (data) => {         
     $(".tekst").text(data.pitanje_1);
     $(".sledece_pitanje").css("display", "inline");
@@ -49,6 +48,35 @@ function ucitajPitanje1() {
       $("#odgovor_treci").attr("class", "btn-danger");
       $("#odgovor_cetvrti").attr("class", "btn-danger");
     });
-    
+});
+}
+function ucitajNovoPitanje(){
+    $.getJSON('pitanja.json', (data) => {         
+      $(".tekst").text(data.pitanje_2);
+      $(".sledece_pitanje").css("display", "inline");
+      $(".izadji_iz_kviza").css("display", "inline");
+      $(".dugme").remove();
+      $("#odgovor_prvi").attr("disabled", false);
+      $("#odgovor_drugi").attr("disabled", false);
+      $("#odgovor_treci").attr("disabled", false);
+      $("#odgovor_cetvrti").attr("disabled", false);
+      $("#odgovor_prvi").attr("class", "btn-success");
+      $("#odgovor_drugi").attr("class", "btn-success");
+      $("#odgovor_treci").attr("class", "btn-success");
+      $("#odgovor_cetvrti").attr("class", "btn-success");
+      $("#odgovor_prvi").html(data.odgovori_2[0]);
+      $("#odgovor_drugi").html(data.odgovori_2[1]);
+      $("#odgovor_treci").html(data.odgovori_2[2]);
+      $("#odgovor_cetvrti").html(data.odgovori_2[3]);
+      $("#odgovor_prvi").css("display", "inline");
+      $("#odgovor_drugi").css("display", "inline");
+      $("#odgovor_treci").css("display", "inline");
+      $("#odgovor_cetvrti").css("display", "inline");
+      $(".odgovor1").click(function(){
+        $("#odgovor_prvi").attr("disabled", "disabled");
+        $("#odgovor_drugi").attr("disabled", "disabled");
+        $("#odgovor_treci").attr("disabled", "disabled");
+        $("#odgovor_cetvrti").attr("disabled", "disabled");
+      });
   });
 }
