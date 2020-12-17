6d4ee68e1ab3804a896bea105f8df6a06b90cf35
Mihailo Jacimovic <mihajac98@gmail.com>
2020-12-17 20:06:31 +0100

    Dodao sam dugme da se izadje iz kviza i dugme da se preskoci pitanje i stilizovao sam ih

--- hw-3/script.js
+++ hw-3/script.js
@@ -1,8 +1,3 @@
-//function mihailo () {
-//  $.getJSON('pitanja.json', (data) => {         
-//    alert(data.pitanje1);
-//  });
-//}
 // funkcije za prelazak sa jedne strane na drugu
 function zapocniKviz() {
     location.replace("http://127.0.0.1:5500/stranica_sa_pitanjima.html");
@@ -27,4 +22,16 @@ function otvoriPravilaIgre() {
 }
 function otvoriFormu() {
   location.replace("http://127.0.0.1:5500/forma.html");
+}
+// funkcije za rad sa pitanjima
+function ucitajPitanje1() {
+  //$(".tekst").remove();
+  $.getJSON('pitanja.json', (data) => {         
+    $(".tekst").text(data.pitanje_1);
+    $(".tekst").attr("id", "prvo_pitanje");
+    $(".sledece_pitanje").css("display", "inline");
+    $(".izadji_iz_kviza").css("display", "inline");
+    $(".dugme").remove();
+    $("#odgovor_prvi").html(data.odgovori_1[0]);
+  });
 }
