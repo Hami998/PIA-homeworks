7773b5993e0e65fa72690a8e0da10f04be7b777c
Mihailo Jacimovic <mihajac98@gmail.com>
2020-12-19 14:51:05 +0100

    Namestio da se ucita sledece pitanje kada se klikne na dugme sledece pitanje

--- hw-3/script.js
+++ hw-3/script.js
@@ -24,9 +24,14 @@ function otvoriFormu() {
   location.replace("http://127.0.0.1:5500/forma.html");
 }
 // funkcije za rad sa pitanjima
+//let brojac = 1;
+//function povecajBrojac(){
+//  brojac += 1;
+//}
 function ucitajPrvoPitanje(){
   $.getJSON('pitanja.json', (data) => {         
     $(".tekst").text(data.pitanje_1);
+    $(".brojac").text("2");
     $(".sledece_pitanje").css("display", "inline");
     $(".izadji_iz_kviza").css("display", "inline");
     $(".dugme").remove();
@@ -51,32 +56,78 @@ function ucitajPrvoPitanje(){
 });
 }
 function ucitajNovoPitanje(){
-    $.getJSON('pitanja.json', (data) => {         
-      $(".tekst").text(data.pitanje_2);
-      $(".sledece_pitanje").css("display", "inline");
-      $(".izadji_iz_kviza").css("display", "inline");
-      $(".dugme").remove();
-      $("#odgovor_prvi").attr("disabled", false);
-      $("#odgovor_drugi").attr("disabled", false);
-      $("#odgovor_treci").attr("disabled", false);
-      $("#odgovor_cetvrti").attr("disabled", false);
-      $("#odgovor_prvi").attr("class", "btn-success");
-      $("#odgovor_drugi").attr("class", "btn-success");
-      $("#odgovor_treci").attr("class", "btn-success");
-      $("#odgovor_cetvrti").attr("class", "btn-success");
-      $("#odgovor_prvi").html(data.odgovori_2[0]);
-      $("#odgovor_drugi").html(data.odgovori_2[1]);
-      $("#odgovor_treci").html(data.odgovori_2[2]);
-      $("#odgovor_cetvrti").html(data.odgovori_2[3]);
-      $("#odgovor_prvi").css("display", "inline");
-      $("#odgovor_drugi").css("display", "inline");
-      $("#odgovor_treci").css("display", "inline");
-      $("#odgovor_cetvrti").css("display", "inline");
-      $(".odgovor1").click(function(){
-        $("#odgovor_prvi").attr("disabled", "disabled");
-        $("#odgovor_drugi").attr("disabled", "disabled");
-        $("#odgovor_treci").attr("disabled", "disabled");
-        $("#odgovor_cetvrti").attr("disabled", "disabled");
-      });
-  });
+  let brojac = $(".brojac").text();
+    if(brojac==2){
+      $.getJSON('pitanja.json', (data) => {         
+        $(".tekst").text(data.pitanje_2);
+        $(".brojac").text("3");
+        $(".sledece_pitanje").css("display", "inline");
+        $(".izadji_iz_kviza").css("display", "inline");
+        $(".dugme").remove();
+        $("#odgovor_prvi").attr("disabled", false);
+        $("#odgovor_drugi").attr("disabled", false);
+        $("#odgovor_treci").attr("disabled", false);
+        $("#odgovor_cetvrti").attr("disabled", false);
+        $("#odgovor_prvi").attr("class", "btn-success");
+        $("#odgovor_drugi").attr("class", "btn-success");
+        $("#odgovor_treci").attr("class", "btn-success");
+        $("#odgovor_cetvrti").attr("class", "btn-success");
+        $("#odgovor_prvi").html(data.odgovori_2[0]);
+        $("#odgovor_drugi").html(data.odgovori_2[1]);
+        $("#odgovor_treci").html(data.odgovori_2[2]);
+        $("#odgovor_cetvrti").html(data.odgovori_2[3]);
+        $("#odgovor_prvi").css("display", "inline");
+        $("#odgovor_drugi").css("display", "inline");
+        $("#odgovor_treci").css("display", "inline");
+        $("#odgovor_cetvrti").css("display", "inline");
+        $("button").removeClass("odgovor");
+        $("button").addClass("odgovor_2");
+        $(".odgovor_2").click(function(){
+          $("#odgovor_prvi").attr("disabled", "disabled");
+          $("#odgovor_drugi").attr("disabled", "disabled");
+          $("#odgovor_treci").attr("disabled", "disabled");
+          $("#odgovor_cetvrti").attr("disabled", "disabled");
+          $("#odgovor_prvi").attr("class", "btn-success");
+          $("#odgovor_drugi").attr("class", "btn-danger");
+          $("#odgovor_treci").attr("class", "btn-danger");
+          $("#odgovor_cetvrti").attr("class", "btn-danger");
+        });
+    });
+  }
+    if(brojac == 3){
+      $.getJSON('pitanja.json', (data) => {
+        $(".tekst").text(data.pitanje_4);
+        $(".sledece_pitanje").css("display", "inline");
+        $(".izadji_iz_kviza").css("display", "inline");
+        $(".dugme").remove();
+        $("#odgovor_prvi").attr("disabled", false);
+        $("#odgovor_drugi").attr("disabled", false);
+        $("#odgovor_treci").attr("disabled", false);
+        $("#odgovor_cetvrti").attr("disabled", false);
+        $("#odgovor_prvi").attr("class", "btn-success");
+        $("#odgovor_drugi").attr("class", "btn-success");
+        $("#odgovor_treci").attr("class", "btn-success");
+        $("#odgovor_cetvrti").attr("class", "btn-success");
+        $("#odgovor_prvi").html(data.odgovori_4[0]);
+        $("#odgovor_drugi").html(data.odgovori_4[1]);
+        $("#odgovor_treci").html(data.odgovori_4[2]);
+        $("#odgovor_cetvrti").html(data.odgovori_4[3]);
+        $("#odgovor_prvi").css("display", "inline");
+        $("#odgovor_drugi").css("display", "inline");
+        $("#odgovor_treci").css("display", "inline");
+        $("#odgovor_cetvrti").css("display", "inline");
+        $("button").removeClass("odgovor_2");
+        $("button").addClass("odgovor_4");
+        $(".odgovor_4").click(function(){
+          $("#odgovor_prvi").attr("disabled", "disabled");
+          $("#odgovor_drugi").attr("disabled", "disabled");
+          $("#odgovor_treci").attr("disabled", "disabled");
+          $("#odgovor_cetvrti").attr("disabled", "disabled");
+          $("#odgovor_prvi").attr("class", "btn-danger");
+          $("#odgovor_drugi").attr("class", "btn-danger");
+          $("#odgovor_treci").attr("class", "btn-success");
+          $("#odgovor_cetvrti").attr("class", "btn-danger");
+        });    
+      });    
+    }
 }
