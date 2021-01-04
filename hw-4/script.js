5def212ecaa7ac7842840a3992170b3d6130b903
Mihailo Jacimovic <mihajac98@gmail.com>
2021-01-05 00:12:16 +0100

    Napravio modal za Sign Up i funkcije openSignUp(), closeSignUp()

--- hw-4/script.js
+++ hw-4/script.js
@@ -5,3 +5,9 @@ function openSignIn() {
 function closeSignIn() {
     $(".sign_in_modal_bg").removeClass("sign_in_modal_bg_active");
 }
+function openSignUp() {
+    $(".sign_up_modal_bg").addClass("sign_up_modal_bg_active");
+}
+function closeSignUp() {
+$(".sign_up_modal_bg").removeClass("sign_up_modal_bg_active");
+}
