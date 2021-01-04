bc4e7973866fd1f72da9589e684129578f025091
Mihailo Jacimovic <mihajac98@gmail.com>
2021-01-04 23:47:20 +0100

    Dodao modal Sign in i funkcije openSignIn(), closeSignIn()

---
+++ hw-4/script.js
@@ -0,0 +1,7 @@
+// funkcije za otvaranje formi
+function openSignIn() {
+        $(".sign_in_modal_bg").addClass("sign_in_modal_bg_active");
+}
+function closeSignIn() {
+    $(".sign_in_modal_bg").removeClass("sign_in_modal_bg_active");
+}
