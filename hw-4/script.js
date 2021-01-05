4c575737f600e595f467c55044b767054d4630cc
Mihailo Jacimovic <mihajac98@gmail.com>
2021-01-05 01:32:59 +0100

    Ispitao sve uslove u funkciji checkSignIn()

--- hw-4/script.js
+++ hw-4/script.js
@@ -23,13 +23,17 @@ function checkSignUp(){
     surname = $("#surname").val();
     nick = $("#nick").val();
     email = $("#email").val();
-    password = $("#password").val();
-    passwordAagain = $("#password_again").val();
+    password = $("#password_1").val();
+    passwordAgain = $("#password_again").val();
     // let pattern = /^[A-Z]{1}[a-z]*$/;
     let pattern_0 = /^\s*$/;
     let pattern_1 = /^[A-Z]{1}/;
     let pattern_2 = /^[A-Z]{1}[a-z]*$/;
     let pattern_3 = /^[a-zA-Z0-9]*$/;
+    let pattern_4 = /^[a-zA-Z]{1}[a-zA-Z0-9]*@gmail.com$/;
+    let pattern_5 = /[A-Z]/;
+    let pattern_6 = /[a-z]/;
+    let pattern_7 = /[0-9]/;
     let checkName_0 = pattern_0.exec(name);
     let checkName_1 = pattern_1.exec(name);
     let checkName_2 = pattern_2.exec(name);
@@ -38,6 +42,13 @@ function checkSignUp(){
     let checkSurname_2 = pattern_2.exec(surname);
     let checkNick_0 = pattern_0.exec(nick);
     let checkNick_1 = pattern_3.exec(nick);
+    let checkEmail_0 = pattern_0.exec(email);
+    let checkEmail_1 = pattern_4.exec(email);
+    let checkPassword_1 = pattern_0.exec(password);
+    let checkPassword_2 = pattern_5.exec(password);
+    let checkPassword_3 = pattern_6.exec(password);
+    let checkPassword_4 = pattern_7.exec(password);
+    let checkCorrectPassword = password.localeCompare(passwordAgain);
     if(checkName_0){
         alert("You should enter your first name");
         $("#name").val("");
@@ -76,6 +87,41 @@ function checkSignUp(){
     if(!checkNick_1){
         alert("You nick should include only letters and numbers, no space");
         $("#nick").val("");
+        return;
+    }
+    if(checkEmail_0){
+        alert("You should enter your email");
+        $("#email").val("");
+        return;
+    }
+    if(!checkEmail_1){
+        alert("You should enter your email in format johnsmith990@gmail.com");
+        $("#email").val("");
+        return;
+    }
+    if(checkPassword_1){
+        alert("You should enter password, it should contain capital and small letters and numbers");
+        $("#password_1").val("");
+        return;
+    }
+    if(!checkPassword_2){
+        alert("Password is weak, it should contain capital letters");
+        $("#password_1").val("");
+        return;
+    }
+    if(!checkPassword_3){
+        alert("Password is weak, it should contain small letters");
+        $("#password_1").val("");
+        return;
+    }
+    if(!checkPassword_4){
+        alert("Password is weak, it should contain numbers");
+        $("#password_1").val("");
+        return;
+    }
+    if(checkCorrectPassword == 1 || checkCorrectPassword == -1){
+        alert("Password doesn't match");
+        $("#password_again").val("");
         return;
     }
 }
