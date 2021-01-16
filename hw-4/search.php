e2f32eecd4b79f1c1045842916600c0ca5b917aa
Mihailo Jacimovic <mihajac98@gmail.com>
2021-01-16 19:43:56 +0100

    Dodao da se filmovi biraju po zanru

--- hw-4/search.php
+++ hw-4/search.php
@@ -2,16 +2,6 @@
     session_start();
     $_SESSION;
     include ("connection_database.php");
-    $query_3 = "SELECT title FROM movie_list;";
-    $result = mysqli_query($connected, $query_3);
-
-    echo "<div class=\"list\">";
-    echo "<ul class=\" list_list\"";
-    while($row = mysqli_fetch_array($result)){
-        echo "<li class=\"list_dot\">" . $row['title'] . "</li>";
-    }
-    echo "</ul>";
-    echo "</div>";
 ?>
 <!DOCTYPE html>
 <html>
@@ -25,21 +15,54 @@
         <link rel="stylesheet" href="css/bootstrap.css">
         <link rel="stylesheet" href="search.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script type="text/javascript" src="script.js" ></script>
+        <script type="text/javascript" src="script_search.js" ></script>
         <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 </head>
 <body>
     <div class="search_box" id="search_box">
-        <form action="" class="search" id="search" method="post">
+        <form action="result.php" class="search" id="search" method="post">
                 <div class="title_bg">
                     <h3 class="title_movies">Search for the movies you like ...</h3>
                 </div>
                 <div class="form-row rows">
                     <div class="col col_1">
-                    <input type="text" class="movie_name input" id="movie_name" name="movie_name" placeholder="search for movies...">
+                    <input type="text" class="movie_name input" id="movie_name" name="movie_name"
+                     placeholder="search for movies...">
                     </div>
                     <div class="col col_2">
+                    <button type="submit" class="submit_movie btn btn-success">Search</button>
+                    </div>
+                </div>
+                <div class="form-row rows">
+                    <div class="col col_3">
+                    <h3 class="title_movies">Search for the genre you like ...</h3>
+                    </div>
+                    <div class="col col_4">
+                    <input type="checkbox" id="action_movie" name="action_movie" value="action_movie">
+                    <label for="action_movie"> Action movie</label><br>
+                    <input type="checkbox" id="drama_movie" name="drama_movie" value="drama_movie">
+                    <label for="drama_movie"> Drama movie</label><br>
+                    <input type="checkbox" id="horror_movie" name="horror_movie" value="horror_movie">
+                    <label for="horror_movie"> Horror movie</label><br>
+                    <input type="checkbox" id="comedy_movie" name="comedy_movie" value="comedy_movie">
+                    <label for="comedy_movie"> Comedy movie</label><br>
+                    <input type="checkbox" id="sf_movie" name="sf_movie" value="sf_movie">
+                    <label for="sf_movie"> Science fiction movie</label><br>
+                    </div>
+                    <div class="col col_5">
+                    <input type="checkbox" id="action_movie" name="action_movie" value="action_movie">
+                    <label for="action_movie"> Action movie</label><br>
+                    <input type="checkbox" id="drama_movie" name="drama_movie" value="drama_movie">
+                    <label for="drama_movie"> Drama movie</label><br>
+                    <input type="checkbox" id="horror_movie" name="horror_movie" value="horror_movie">
+                    <label for="horror_movie"> Horror movie</label><br>
+                    <input type="checkbox" id="comedy_movie" name="comedy_movie" value="comedy_movie">
+                    <label for="comedy_movie"> Comedy movie</label><br>
+                    <input type="checkbox" id="sf_movie" name="sf_movie" value="sf_movie">
+                    <label for="sf_movie"> Science fiction movie</label><br>
+                    </div>
+                    <div class="col col_6">
                     <button type="submit" class="submit_movie btn btn-success">Search</button>
                     </div>
                 </div>
