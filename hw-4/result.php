316a9ff42f915383a95516ba41343c75d37af769
Mihailo Jacimovic <mihajac98@gmail.com>
2021-01-16 20:57:10 +0100

    Kada se izabere zanr za film, prikazuju se svi filmovi tog zanra

--- hw-4/result.php
+++ hw-4/result.php
@@ -2,6 +2,22 @@
     session_start();
     $_SESSION;
     include ("connection_database.php");
+        // if(isset($_POST['movie_name']) and isset($_POST['action_movie']) and isset($_POST['drama_movie'])
+        // and isset($_POST['horror_movie']) and isset($_POST['comedy_movie']) and isset($_POST['sf_movie'])
+        // and isset($_POST['romance_movie']) and isset($_POST['thriller_movie']) and isset($_POST['animation_movie'])
+        // and isset($_POST['mistery_movie']) and isset($_POST['documentary_movie'])){
+        //     echo "Greska";
+        // }
+        if(empty($_POST['movie_name']) and empty($_POST['action_movie']) and empty($_POST['drama_movie'])
+        and empty($_POST['horror_movie']) and empty($_POST['comedy_movie']) and empty($_POST['sf_movie'])
+        and empty($_POST['romance_movie']) and empty($_POST['thriller_movie']) and empty($_POST['animation_movie'])
+        and empty($_POST['mistery_movie']) and empty($_POST['documentary_movie'])){
+            echo "Greska";
+        }
+        if(isset($_POST['movie_name']) and empty($_POST['action_movie']) and empty($_POST['drama_movie'])
+        and empty($_POST['horror_movie']) and empty($_POST['comedy_movie']) and empty($_POST['sf_movie'])
+        and empty($_POST['romance_movie']) and empty($_POST['thriller_movie']) and empty($_POST['animation_movie'])
+        and empty($_POST['mistery_movie']) and empty($_POST['documentary_movie'])){
             $pattern = strtolower($_POST['movie_name']);
             $query_3 = "SELECT title FROM movie_list;";
             $result = mysqli_query($connected, $query_3);
@@ -16,6 +32,193 @@
             }
             echo "</ul>";
             echo "</div>";
+        }
+        else if(empty($_POST['movie_name']) or isset($_POST['action_movie']) or isset($_POST['drama_movie'])
+        or isset($_POST['horror_movie']) or isset($_POST['comedy_movie']) or isset($_POST['sf_movie'])
+        or isset($_POST['romance_movie']) or isset($_POST['thriller_movie']) or isset($_POST['animation_movie'])
+        or isset($_POST['mistery_movie']) or isset($_POST['documentary_movie'])){
+           $action = "1";
+           if(isset($_POST['action_movie'])){
+            $action = $_POST['action_movie'];
+           }
+           $drama = "1";
+           if(isset($_POST['drama_movie'])){
+            $drama = $_POST['drama_movie'];
+           }
+           $horror = "1";
+           if(isset($_POST['horror_movie'])){
+            $horror = $_POST['horror_movie'];
+           }
+           $comedy = "1";
+           if(isset($_POST['comedy_movie'])){
+            $comedy = $_POST['comedy_movie'];
+           }
+           $sf = "1";
+           if(isset($_POST['sf_movie'])){
+            $sf = $_POST['sf_movie'];
+           }
+           $romance = "1";
+           if(isset($_POST['romance_movie'])){
+           $romance = $_POST['romance_movie'];
+           }
+           $thriller = "1";
+           if(isset($_POST['thriller_movie'])){
+            $thriller = $_POST['thriller_movie'];
+            }
+           $animation = "1";
+           if(isset($_POST['animation_movie'])){
+            $animation = $_POST['animation_movie'];
+            }
+           $mistery = "1";
+           if(isset($_POST['mistery_movie'])){
+            $mistery = $_POST['mistery_movie'];
+            }
+           $documentary = "1";
+           if(isset($_POST['documentary_movie'])){
+            $documentary = $_POST['documentary_movie'];
+            }
+           echo "$action $documentary $drama  $mistery $thriller $romance";
+           $query_4 = "SELECT title, genre FROM movie_list;";
+           $result_1 = mysqli_query($connected, $query_4);
+           $string_result_1 = "";
+           $string_result_2 = "";
+           $string_result_3 = "";
+           $string_result_4 = "";
+           $string_result_5 = "";
+           $string_result_6 = "";
+           $string_result_7 = "";
+           $string_result_8 = "";
+           $string_result_9 = "";
+           $string_result_10 = "";
+           echo "<div class=\"list\">";
+           echo "<ul class=\" list_list\"";
+
+           while($row = mysqli_fetch_array($result_1)){
+               $genre = $row['genre'];
+               if($action !== "1"){
+                $string_result_1 = strpos($genre,$action);
+                if($string_result_1 === FALSE){
+                    $string_result_1 = -1;
+                }
+               }
+               else{
+                $string_result_1 = -1;
+               }
+               if($drama !== "1"){
+                $string_result_2 = strpos($genre,$drama);
+                if($string_result_2 === FALSE){
+                    $string_result_2 = -1;
+                }
+               }
+               else{
+                $string_result_2 = -1;
+               }
+               if($horror !== "1"){
+                $string_result_3 = strpos($genre,$horror);
+                if($string_result_3 === FALSE){
+                    $string_result_3 = -1;
+                }
+               }
+               else{
+                $string_result_3 = -1;
+               }
+               if($comedy !== "1"){
+                $string_result_4 = strpos($genre,$comedy);
+                if($string_result_4 === FALSE){
+                    $string_result_4 = -1;
+                }
+               }
+               else{
+                $string_result_4 = -1;
+               }
+               if($sf !== "1"){
+                $string_result_5 = strpos($genre,$sf);
+                if($string_result_5 === FALSE){
+                    $string_result_5 = -1;
+                }
+               }
+               else{
+                $string_result_5 = -1;
+               }
+               if($romance !== "1"){
+                $string_result_6 = strpos($genre,$romance);
+                if($string_result_6 === FALSE){
+                    $string_result_6 = -1;
+                }
+               }
+               else{
+                $string_result_6 = -1;
+               }
+               if($thriller !== "1"){
+                $string_result_7 = strpos($genre,$thriller);
+                if($string_result_7 === FALSE){
+                    $string_result_7 = -1;
+                }
+               }
+               else{
+                $string_result_7 = -1;
+               }
+               if($animation !== "1"){
+                $string_result_8 = strpos($genre,$animation);
+                if($string_result_8 === FALSE){
+                    $string_result_8 = -1;
+                }
+               }
+               else{
+                $string_result_8 = -1;
+               }
+               if($mistery !== "1"){
+                $string_result_9 = strpos($genre,$mistery);
+                if($string_result_9 === FALSE){
+                    $string_result_9 = -1;
+                }
+               }
+               else{
+                $string_result_9 = -1;
+               }
+               if($documentary !== "1"){
+                $string_result_10 = strpos($genre,$documentary);
+                if($string_result_10 === FALSE){
+                    $string_result_10 = -1;
+                }
+               }
+               else{
+                $string_result_10 = -1;
+               }
+               if( $string_result_1 >= 0){
+                   echo "<li class=\"list_dot\" \"list_item\">" . $row['title'] . "</li>";
+               }
+               else if( $string_result_2 >= 0){
+                echo "<li class=\"list_dot\" \"list_item\">" . $row['title'] . "</li>";
+                }
+                else if( $string_result_3 >= 0){
+                    echo "<li class=\"list_dot\" \"list_item\">" . $row['title'] . "</li>";
+                }
+                else if( $string_result_4 >= 0){
+                    echo "<li class=\"list_dot\" \"list_item\">" . $row['title'] . "</li>";
+                }
+                else if( $string_result_5 >= 0){
+                    echo "<li class=\"list_dot\" \"list_item\">" . $row['title'] . "</li>";
+                }
+                else if( $string_result_6 >= 0){
+                    echo "<li class=\"list_dot\" \"list_item\">" . $row['title'] . "</li>";
+                }
+                else if( $string_result_7 >= 0){
+                    echo "<li class=\"list_dot\" \"list_item\">" . $row['title'] . "</li>";
+                }
+                else if( $string_result_8 >= 0){
+                    echo "<li class=\"list_dot\" \"list_item\">" . $row['title'] . "</li>";
+                }
+                else if( $string_result_9 >= 0){
+                    echo "<li class=\"list_dot\" \"list_item\">" . $row['title'] . "</li>";
+                }
+                else if( $string_result_10 >= 0){
+                    echo "<li class=\"list_dot\" \"list_item\">" . $row['title'] . "</li>";
+                }
+           }
+           echo "</ul>";
+           echo "</div>";
+        }
 ?>
 <!DOCTYPE html>
 <html>
