c94eb2f0586fa008c89f8a61d021ea8b6f720294
Mihailo Jacimovic <mihajac98@gmail.com>
2021-01-31 16:11:40 +0100

    Dodao return dugme u result.php i stilizovao stranicu

--- hw-4/result.php
+++ hw-4/result.php
@@ -1,68 +1,74 @@
 <?php
-    session_start();
-    $_SESSION;
     include ("connection_database.php");
     include ("functions.php");
-    $delete = "<button type=\"submit\" class=\"submit_log_off btn btn-success\" 
-    style=\"position:relative;top:0px;left:0px\"> 
-<a href=\"functions.php?log_off=1\" style=\"color:white;text-decoration:none\">Log off</a></button>";
+    $_SESSION;
+    if($_SESSION['admin'] == 1 or $_SESSION['admin'] == 0){
+        $log_off = "<button type=\"submit\" class=\"submit_log_off btn btn-success\"\"> 
+        <a href=\"functions.php?log_off=1\" style=\"color:white;text-decoration:none\">Log off</a></button>";
+        $return = "<button type=\"submit\" class=\"submit_return btn btn-success\" \"> 
+        <a href=\"search.php\" style=\"color:white;text-decoration:none\">Return</a></button>";
+    }
+$movie_list = "";
     $user_connection = check_login($connected);
         if(empty($_POST['movie_name']) and empty($_POST['action_movie']) and empty($_POST['drama_movie'])
         and empty($_POST['horror_movie']) and empty($_POST['comedy_movie']) and empty($_POST['sf_movie'])
         and empty($_POST['romance_movie']) and empty($_POST['thriller_movie']) and empty($_POST['animation_movie'])
         and empty($_POST['mistery_movie']) and empty($_POST['documentary_movie'])){
-            $query_3 = "SELECT imdb_id, title FROM movie_list;";
+            $query_3 = 
+            "SELECT movie_list_new.*, movie_poster.poster FROM movie_list_new 
+            INNER JOIN movie_poster ON movie_list_new.imdb_id = concat('tt', movie_poster.imdbID)
+            ORDER BY year DESC, title";
             $result = mysqli_query($connected, $query_3);
-            $movie_list = "";
+            $_SESSION['id'] = '';
             while($row = mysqli_fetch_array($result)){
                 if ($_SESSION['admin'] == 1){      
-                    $movie_list .= "<div class='list_item'><a class=\"link\" style=\"color:black;text-decoration:none\"
+                    $movie_list .= "<div class='list_item'><a class=\"link_1\"\"
                     href=\"movie.php?id=" . $row['imdb_id'] . "\">" . $row['title'] . "</a> </div>
-                     <button class=\"delete_button\">
-                     <a class=\"link\" style=\"color:white;text-decoration:none\" 
-                     href=\"functions.php?title=" . $row['title'] . "\">Delate</button>";
+                    <div class='delete_button'><a class=\"link\" \"
+                    href=\"functions.php?title=" . $row['title'] . "\">Delate</div>";
                 }   
                 else{
-                    $movie_list .= "<div class='list_item'><a href=\"movie.php?id=" . $row['imdb_id'] . "\">" . $row['title'] . "</a></div>";
+                    $movie_list .= "<div class='list_item'><a class=\"link_1\"\"
+                    href=\"movie.php?id=" . $row['imdb_id'] . "\">" . $row['title'] . "</a> </div>";
                 }           
             }
         }
-        // if(empty($_POST['movie_name']) and empty($_POST['action_movie']) and empty($_POST['drama_movie'])
-        // and empty($_POST['horror_movie']) and empty($_POST['comedy_movie']) and empty($_POST['sf_movie'])
-        // and empty($_POST['romance_movie']) and empty($_POST['thriller_movie']) and empty($_POST['animation_movie'])
-        // and empty($_POST['mistery_movie']) and empty($_POST['documentary_movie'])){
-        //     $query_3 = "SELECT imdb_id, title FROM movie_list;";
-        //     $result = mysqli_query($connected, $query_3);
-        //     while($row = mysqli_fetch_array($result)){
-        //         if ($_SESSION['admin'] == 1){      
-        //             $movie_list = "<div class='list_item'><a class=\"link\" href=\"movie.php?id=" . $row['imdb_id'] . "\">" . $row['title'] . "</a> </div>
-        //              <button class=\"delete_button\"><a class=\"link\" href=\"functions.php?title=" . $row['title'] . "\">Delate</button>";
-        //         }   
-        //         else{
-        //             $movie_list = "<div class='list_item'><a href=\"movie.php?id=" . $row['imdb_id'] . "\">" . $row['title'] . "</a></div>";
-        //         }                   
-        //         }
-        // }
-        else if(isset($_POST['movie_name']) and empty($_POST['action_movie']) and empty($_POST['drama_movie'])
-        and empty($_POST['horror_movie']) and empty($_POST['comedy_movie']) and empty($_POST['sf_movie'])
-        and empty($_POST['romance_movie']) and empty($_POST['thriller_movie']) and empty($_POST['animation_movie'])
-        and empty($_POST['mistery_movie']) and empty($_POST['documentary_movie'])){
+        else if(isset($_POST['movie_name']) && empty($_POST['action_movie']) && empty($_POST['drama_movie'])
+        && empty($_POST['horror_movie']) && empty($_POST['comedy_movie']) && empty($_POST['sf_movie'])
+        && empty($_POST['romance_movie']) && empty($_POST['thriller_movie']) && empty($_POST['animation_movie'])
+        && empty($_POST['mistery_movie']) && empty($_POST['documentary_movie'])){
             $pattern = strtolower($_POST['movie_name']);
-            $query_3 = "SELECT title FROM movie_list;";
+            $query_3 = 
+            "SELECT movie_list_new.*, movie_poster.poster FROM movie_list_new 
+            INNER JOIN movie_poster ON movie_list_new.imdb_id = concat('tt', movie_poster.imdbID)
+            ORDER BY year DESC, title";
             $result = mysqli_query($connected, $query_3);
             while($row = mysqli_fetch_array($result)){
                 $title = strtolower($row['title']);
                 $string_result = strpos($title,$pattern);
                 if( $string_result === 0){
-                    echo " <div class=\"list_item\">" .  $row['title'] . "</div>";
+                    if ($_SESSION['admin'] == 1){      
+                        $movie_list .= "<div class='list_item'><a class=\"link_1\"\"
+                        href=\"movie.php?id=" . $row['imdb_id'] . "\">" . $row['title'] . "</a> </div>
+                        <div class='delete_button'><a class=\"link\" \"
+                        href=\"functions.php?title=" . $row['title'] . "\">Delate</div>";
+                    }   
+                    else{
+                        $movie_list .= "<div class='list_item'><a class=\"link_1\"\"
+                        href=\"movie.php?id=" . $row['imdb_id'] . "\">" . $row['title'] . "</a> </div>";
+                    }    
                 }
             }
         }
-        else if(empty($_POST['movie_name']) or isset($_POST['action_movie']) or isset($_POST['drama_movie'])
+        else if(isset($_POST['movie_name']) or isset($_POST['action_movie']) or isset($_POST['drama_movie'])
         or isset($_POST['horror_movie']) or isset($_POST['comedy_movie']) or isset($_POST['sf_movie'])
         or isset($_POST['romance_movie']) or isset($_POST['thriller_movie']) or isset($_POST['animation_movie'])
         or isset($_POST['mistery_movie']) or isset($_POST['documentary_movie'])){
            $action = "1";
+           $movie_name = "";
+           if(isset($_POST['movie_name'])){
+            $movie_name = $_POST['movie_name'];
+           }
            if(isset($_POST['action_movie'])){
             $action = $_POST['action_movie'];
            }
@@ -102,9 +108,11 @@
            if(isset($_POST['documentary_movie'])){
             $documentary = $_POST['documentary_movie'];
             }
-           echo "$action $documentary $drama  $mistery $thriller $romance";
-           $query_4 = "SELECT imdb_id, title, genre FROM movie_list;";
-           $result_1 = mysqli_query($connected, $query_4);
+            $query_3 = 
+            "SELECT movie_list_new.*, movie_poster.poster FROM movie_list_new 
+            INNER JOIN movie_poster ON movie_list_new.imdb_id = concat('tt', movie_poster.imdbID)
+            ORDER BY year DESC, title";
+            $result_1 = mysqli_query($connected, $query_3);
            $string_result_1 = "";
            $string_result_2 = "";
            $string_result_3 = "";
@@ -251,19 +259,39 @@
                 $string_result_10 = -1;
                }
                if($checked_number === $checked_genre){
-                if ($_SESSION['admin'] == 1){      
-                    echo "<div class='list_item'><a class=\"link\" href=\"movie.php?id=" . $row['imdb_id'] . "\">" . $row['title'] . "</a> </div>
-                     <button class=\"delete_button\"><a class=\"link\" href=\"functions.php?title=" . $row['title'] . "\">Delate</button>";
-                }   
-                else{
-                    echo "<div class='list_item'><a href=\"movie.php?id=" . $row['imdb_id'] . "\">" . $row['title'] . "</a></div>";
+                   if(strlen($movie_name)> 0){
+                    $title = strtolower($row['title']);
+                    $pattern = strtolower($movie_name);
+                    $string_result = strpos($title,$pattern);
+                    if( $string_result === 0){
+                        if ($_SESSION['admin'] == 1){      
+                            $movie_list .= "<div class='list_item'><a class=\"link_1\"\"
+                            href=\"movie.php?id=" . $row['imdb_id'] . "\">" . $row['title'] . "</a> </div>
+                            <div class='delete_button'><a class=\"link\" \"
+                            href=\"functions.php?title=" . $row['title'] . "\">Delate</div>";
+                        }   
+                        else{
+                            $movie_list .= "<div class='list_item'><a class=\"link_1\"\"
+                            href=\"movie.php?id=" . $row['imdb_id'] . "\">" . $row['title'] . "</a> </div>";
+                        }    
+                   }
                 }
-                // echo "<div class='list_item'><a href=\"movie.php?id=$row['imdb_id']\">$row['title']</a></div>";
-
+                else{
+                    if ($_SESSION['admin'] == 1){      
+                        $movie_list .= "<div class='list_item'><a class=\"link_1\"\"
+                        href=\"movie.php?id=" . $row['imdb_id'] . "\">" . $row['title'] . "</a> </div>
+                        <div class='delete_button'><a class=\"link\" \"
+                        href=\"functions.php?title=" . $row['title'] . "\">Delate</div>";
+                    }   
+                    else{
+                        $movie_list .= "<div class='list_item'><a class=\"link_1\"\"
+                        href=\"movie.php?id=" . $row['imdb_id'] . "\">" . $row['title'] . "</a> </div>";
+                    }     
                }
            }
            echo "</form>";
         }
+    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -284,7 +312,11 @@
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 </head>
 <body>
-    <?php  echo  $delete;  ?>
-    <?php  echo  $movie_list;  ?>
+    <?php echo $log_off;?>
+        <?php   
+                    echo  $movie_list;
+                    echo $return;
+        ?>
+
 </body>
 </html>
