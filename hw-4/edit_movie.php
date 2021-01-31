b541040c346e05291c4dde1da122ba67ba82c958
Mihailo Jacimovic <mihajac98@gmail.com>
2021-01-31 01:55:47 +0100

    Podesio uslove pod kojim podaci mogu da se upisu u bazu i ispravno prikazu

--- hw-4/edit_movie.php
+++ hw-4/edit_movie.php
@@ -5,8 +5,156 @@
     $user_connection = check_login($connected);
     $id = $_GET['movie_id'];
     if($_SESSION['admin'] == 0){
-        header("location: movie.php?id=". $id);
+        header("location: index.php");
     }
+    if ($_SERVER['REQUEST_METHOD'] = 'POST') {
+        if(isset($_POST['submit'])){
+        $title = ucfirst(strtolower($_POST['title']));
+        $year = $_POST['year'];
+        $date_of_publishing = $_POST['date_of_publishing'];
+        $genre = $_POST['genre'];
+        $duration = $_POST['duration'];
+        $director = ucfirst(strtolower($_POST['director']));
+        $writer = ucfirst(strtolower($_POST['writer']));
+        $production_com = ucfirst(strtolower($_POST['production_com']));
+        $description = $_POST['description'];
+        $rating = $_POST['rating'];
+        $voters = $_POST['voters'];
+        $actors = $_POST['actors'];
+        $imgName_1 = $_FILES['file']['name'];
+        if(!empty($title)){
+            $query = "UPDATE movie_list SET title='$title' WHERE imdb_id='$id'";
+            $result = mysqli_query($connected, $query);
+        }
+        if(!empty($description)){
+            $query = "UPDATE movie_list SET description='$description' WHERE imdb_id='$id'";
+            $result = mysqli_query($connected, $query);
+        }
+        if(!empty($genre)){
+            $check = 0;
+            $array_of_genre = explode(",", $genre);
+            foreach($array_of_genre as $genre_1){
+                if(is_numeric($genre_1)){
+                    $check++;
+                }
+            }
+            if($check == 0){
+                $query = "UPDATE movie_list SET genre='$genre' WHERE imdb_id='$id'";
+                $result = mysqli_query($connected, $query);
+            }
+            else{
+                echo 'alert(Genre is not well written!!)';
+            }
+        }
+        if(!empty($year)){
+            if(is_numeric($year)){
+                $query = "UPDATE movie_list SET year='$year' WHERE imdb_id='$id'";
+                $result = mysqli_query($connected, $query);
+            }
+            else{
+                echo 'alert(Year is supposed to be a number!!)';
+            }
+        }
+        if(!empty($date_of_publishing)){
+            $array_of_date = explode(".", $date_of_publishing);
+            $array_of_date_1 = explode("-", $date_of_publishing);
+            if(checkdate ( intval($array_of_date[0]) , intval($array_of_date[1]) , intval($array_of_date[2]))){
+                $query = "UPDATE movie_list SET data_published='$date_of_publishing' WHERE imdb_id='$id'";
+                $result = mysqli_query($connected, $query);
+            }
+            else if(checkdate ( intval($array_of_date_1[2]) , intval($array_of_date_1[1]) , intval($array_of_date_1[0]))) {
+                $query = "UPDATE movie_list SET data_published='$date_of_publishing' WHERE imdb_id='$id'";
+                $result = mysqli_query($connected, $query);
+            }
+            else{
+                echo "alert(Date is in wrong format, try dd.mm.yyy)";
+            }
+        }
+        if(!empty($duration)){
+            if(is_numeric($duration)){
+                $query = "UPDATE movie_list SET duration='$duration' WHERE imdb_id='$id'";
+                $result = mysqli_query($connected, $query);
+            }
+            else{
+                echo "alert(Duration should be only number)";
+            }
+        }
+        if(!empty($director)){
+            if(!(is_numeric($director))){
+                $query = "UPDATE movie_list SET director='$director' WHERE imdb_id='$id'";
+                $result = mysqli_query($connected, $query);
+            }
+            else{
+                echo "alert(You didnn't write director name correctly)"; 
+            }
+        }
+        if(!empty($writer)){
+            if(!(is_numeric($writer))){
+                $query = "UPDATE movie_list SET writer='$writer' WHERE imdb_id='$id'";
+                $result = mysqli_query($connected, $query);
+            }
+            else{
+                echo "alert(You didnn't write writer name correctly)"; 
+            }
+        }
+        if(!empty($production_com)){
+            if(is_numeric($production_com)){
+                echo "alert(You didnn't write production company name correctly)"; 
+                $query = "UPDATE movie_list SET prodction_company='$production_com' WHERE imdb_id='$id'";
+                $result = mysqli_query($connected, $query);
+            }
+            else{
+                $query = "UPDATE movie_list SET production_company='$production_com' WHERE imdb_id='$id'";
+                $result = mysqli_query($connected, $query);
+            }
+        }
+        if(!empty($rating)){
+            if(is_numeric($rating) && $rating <= 10 && $rating > 0){
+                $query = "UPDATE movie_list SET avg_votes='$rating' WHERE imdb_id='$id'";
+                $result = mysqli_query($connected, $query);
+            }
+            else{
+                echo "alert(Rating should be number between 0 and 10)"; 
+            }
+        }
+        if(!empty($voters)){
+            if(is_numeric($voters) && $voters >= 0){
+                $query = "UPDATE movie_list SET votes='$voters' WHERE imdb_id='$id'";
+                $result = mysqli_query($connected, $query);
+            }
+            else{
+                echo "alert(Voters is a number greater or equal to 0)"; 
+            }
+        }
+        $check = 0;
+        if(!empty($actors)){
+            $array_of_actors = explode(".", $actors);  
+            foreach($array_of_actors as $actor){
+                if(!(is_numeric($actor))){
+                    continue;
+                }
+                else{
+                    $check++;
+                }
+            }
+            if($check == 0){
+                $query = "UPDATE movie_list SET actors='$actors' WHERE imdb_id='$id'";
+                $result = mysqli_query($connected, $query);
+            }
+        }
+        if(!empty($imgName_1)){
+            $add = 1;
+            $id_new = str_replace("tt","",$id);
+            $query = "UPDATE movie_poster SET poster='$imgName_1', added='$add' WHERE imdbID='$id_new'";
+            $result = mysqli_query($connected, $query);
+            $fileDestination = "movie_images/" . $imgName_1;
+            move_uploaded_file($_FILES['file']['tmp_name'], $fileDestination);
+        }
+        header("Location: movie.php?id=". $id);
+        }
+        }
+
+
     $logoff = "<button type=\"submit\" class=\"submit_log_off btn btn-success\" 
     style=\"position:absolute;top:0px;left:0px;width:100px\"> 
     <a href=\"functions.php?log_off=1\" style=\"color:white;text-decoration:none\">Log off</a></button>";
@@ -46,67 +194,7 @@
             if($_SESSION['admin'] == 1){
                 $delete_movie = "<button type=\"submit\" class=\"submit_movie admin_btn submit_all btn btn-success\"> 
                 <a href=\"result.php?title=". $title ."\">Delete</a></button>";
-                $save_movie = "<button type=\"submit\" class=\"submit_movie admin_btn submit_all btn btn-success\"> 
-                <a href=\"movie.php?id=". $id ."\">Save</a></button>";
-            }
-            // echo "<div class=\"movie_title\"> <h1>" . $title . "</h1> </div>" ;      
-            // echo "<div class=\"container\">";
-            // echo "<div class=\"basic_informations\">";
-            // echo "<div class=\"col_info\">";
-            // echo "Year: " . $year . "<br>";
-            // echo "Date of publishing movie: " . $data_published . "<br>";
-            // echo "Genre: " . $genre . "<br>";
-            // echo "Duration: " . $duration . " <br>";
-            // echo "Director: " . $director . " <br>";
-            // echo "Writer: " . $writer . " <br>";
-            // echo "Production company: " . $production_company ." <br>";
-            // echo "Description of the movie: <br>";
-            // echo $description ." <br>";
-            // echo "</div>";
-            // echo "<div class=\"col_image\" style=\"background:none;\">";
-            // if($add === "0"){
-            //     echo "<img src=\"" . $link . "\" >";
-            // }
-            // else{
-            //     echo "<img src=\"movie_images/" . $link . "\" height=\"275\" width=\"175\">";
-            // }
-            // // echo "<img src=\"" . $link . "\" >";
-            // echo "</div>";
-            // echo "</div>";
-            // echo "<div class=\"rating\" style=\"display:flex;flex-direction:row;\">
-            // <div class=\"left\" style=\"display:column;flex-basis:80%;margin-left:0px;margin-bottom:10px;\">
-            // Rating of the movie: <button class=\"votes\" disabled>" . $avg_votes . "</button></br>";
-            // echo "Number of voters: ";
-            // echo $votes . " <br> ";
-            // echo "<div class=\"choose\">
-            // <ul class=\"list\">
-            // <li>Rate the movie</li>
-            //   <ul>
-            //       <li class=\"list_item_5\"><a href=\"functions.php?rating=1&title_1=" . $title . "\"> 1 star </a></li>
-            //       <li class=\"list_item_4\"><a href=\"functions.php?rating=2&title_1=" . $title . "\"> 2 star </a></li>
-            //       <li class=\"list_item_3\"><a href=\"functions.php?rating=3&title_1=" . $title . "\"> 3 star </a></li>
-            //       <li class=\"list_item_2\"><a href=\"functions.php?rating=4&title_1=" . $title . "\"> 4 star </a></li>
-            //       <li class=\"list_item_1\"><a href=\"functions.php?rating=5&title_1=" . $title . "\"> 5 star </a></li>
-            //       <li class=\"list_item_1\"><a href=\"functions.php?rating=6&title_1=" . $title . "\"> 6 star </a></li>
-            //       <li class=\"list_item_1\"><a href=\"functions.php?rating=7&title_1=" . $title . "\"> 7 star </a></li>
-            //       <li class=\"list_item_1\"><a href=\"functions.php?rating=8&title_1=" . $title . "\"> 8 star </a></li>
-            //       <li class=\"list_item_1\"><a href=\"functions.php?rating=9&title_1=" . $title . "\"> 9 star </a></li>
-            //       <li class=\"list_item_1\"><a href=\"functions.php?rating=10&title_1=" . $title . "\"> 10 star </a></li>
-            //   </ul>
-            // </ul>
-            // </div>";
-            // echo "</div>";
-            // echo " </div> ";
-            // echo "<div class=\"right\" style=\"display:column;flex-basis:20%;\">";
-            // echo "</div>";
-            // $array_actors = (explode(", ",$actors));
-            // echo "<div class=\"actors\">";
-            // echo "<h4>Cast overview: </h4><br>";
-            // foreach($array_actors as $actor){
-            //     echo "<div  class=\"actor\">" . $actor . "</div>";
-            // }
-            // echo "</div>";
-            // echo "</div>";      
+            }  
     }
 }
 ?>
@@ -218,13 +306,13 @@
             <textarea type="text" class="actors textarea" id="actors" 
             name="actors"></textarea>
             <br>
-            <label for="img" class="label">Img link: </label>
+            <label for="file" class="label">Img link: </label>
             <input type="file" name="file">
             <br>
             </div>
             </div>
-            <button type="submit" name="submit" class="preview_movie btn btn-success" onclick="addMovie()">
-            Preview</button>
+            <button type="submit" name="submit" class="preview_movie btn btn-success">
+            Save</button>
             </form>
             </div>    
 </body>
